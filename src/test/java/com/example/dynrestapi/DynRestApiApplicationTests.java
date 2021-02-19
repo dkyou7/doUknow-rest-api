9c24837daa9e24b059187d83b8c99d4e415947f5
youdongkwan <dkyou7@naver.com>
2021-02-19 14:39:18 +0900

    git rebase

---
+++ src/test/java/com/example/dynrestapi/DynRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dynrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
